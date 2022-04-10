e2ecd84ba86834b27e8708011bc75cacf6e28339
Sajib Hossain <sajib.sj359@gmail.com>
2022-04-10 22:49:17 +0600

    create login page with react and establish firebase connection

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQSxeKCkAXf7J7enxZgKz7mtQQRoNASNI",
+  authDomain: "login-page-react-cc2bb.firebaseapp.com",
+  projectId: "login-page-react-cc2bb",
+  storageBucket: "login-page-react-cc2bb.appspot.com",
+  messagingSenderId: "35284178228",
+  appId: "1:35284178228:web:47db4cbdc015025a21c161"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
